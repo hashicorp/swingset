9fda971c7a3a0ad03b62ff39a7687f8a5124730c
Jeff Escalante <jescalan@users.noreply.github.com>
2020-08-26 18:13:00 -0400

    remove unneeded option, add user-defined component

--- example/pages/index.jsx
+++ example/pages/index.jsx
@@ -2,7 +2,6 @@ import createPage from 'octavo/page'
 import createStaticProps from 'octavo/getStaticProps'
 
 const octavoConfig = {
-  componentsRoot: 'components/*',
   additionalComponents: { Tester: () => <p>testing 123</p> },
 }
 
