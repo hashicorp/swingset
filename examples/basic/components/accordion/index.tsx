b50bc42cd42d7a87ee322dd4e892ddca5baec0c5
Preston Bourne <preston.bourne@hashicorp.com>
2023-06-20 09:02:20 -0400

    add tree sorting func

--- examples/basic/components/accordion/index.tsx
+++ examples/basic/components/accordion/index.tsx
@@ -1,15 +1,15 @@
 import { ReactElement } from 'react'
 
 interface AccordionProps {
- color?: string
- children: ReactElement
+  color?: string
+  children: ReactElement
 }
 
 export function Accordion({ children }: AccordionProps) {
- return (
-  <details>
-   <summary>Hello 👋</summary>
-   Nice meeting you! How's everything going?
-  </details>
- )
+  return (
+    <details>
+      <summary>Hello 👋</summary>
+      Nice meeting you! How's everything going?
+    </details>
+  )
 }
