2c902083db390936c62280e2b9a3f87314159de8
Preston Bourne <preston.bourne@hashicorp.com>
2023-06-21 16:05:51 -0400

    make toggle component

---
+++ packages/swingset-theme-hashicorp/src/components/side-nav/toggle-button.tsx
@@ -0,0 +1,22 @@
+import { IconChevronRight24 } from '@hashicorp/flight-icons/svg-react/chevron-right-24'
+import { cx } from 'class-variance-authority'
+
+type ToggleButtonProps = {
+ toggle: () => void
+ isOpen: boolean
+}
+
+export function ToggleButton({ toggle, isOpen }: ToggleButtonProps) {
+ return (
+  <button className="ss-fixed ss-left-72 ss-p-2" onClick={toggle}>
+   <div
+    className={cx(
+     'ss-bg-surface-faint ss-border-faint ss-border-2 ss-rounded-full ss-transition-transform ss-shadow-md ss-p-1 ss-bg-opacity-60 ss-backdrop-blur-sm',
+     !isOpen && 'ss--rotate-180'
+    )}
+   >
+    <IconChevronRight24 />
+   </div>
+  </button>
+ )
+}
