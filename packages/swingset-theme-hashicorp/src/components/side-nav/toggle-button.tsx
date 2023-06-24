0b635386492cfe76e6afbba055d2909899d026ea
Preston Bourne <preston.bourne@hashicorp.com>
2023-06-23 22:36:29 -0400

    a11y changes

--- packages/swingset-theme-hashicorp/src/components/side-nav/toggle-button.tsx
+++ packages/swingset-theme-hashicorp/src/components/side-nav/toggle-button.tsx
@@ -9,9 +9,10 @@ type ToggleButtonProps = {
 export function ToggleButton({ toggle, isOpen }: ToggleButtonProps) {
   return (
     <button
+      aria-label="Toggle the navigation menu between open and closed states"
       className={cx(
-        'ss-bg-surface-faint hover:ss-bg-surface-action hover:ss-text-foreground-action ss-border-2 ss-border-faint hover:ss-border-action ss-p-1 ss-rounded-full ss-transition-transform ss-shadow-md ss-fixed ss-top-6 ss-left-72',
-        !isOpen && 'ss--translate-x-72'
+        'ss-bg-surface-faint ss-hidden lg:ss-block hover:ss-bg-surface-action ss-opacity-70 hover:ss-text-foreground-action ss-border-2 ss-border-faint hover:ss-border-action ss-p-1 ss-rounded-full hover:ss-shadow-lg ss-transition-all ss-shadow-sm ss-fixed ss-top-6 ss-left-72',
+        !isOpen && 'ss--translate-x-[17rem]'
       )}
       onClick={toggle}
     >
