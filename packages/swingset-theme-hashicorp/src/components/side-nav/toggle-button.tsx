08b71a6973f1bef29643f5fa3a3cc289ca24437a
Preston Bourne <preston.bourne@hashicorp.com>
2023-06-26 14:21:48 -0400

    ariaLabelUpdate

--- packages/swingset-theme-hashicorp/src/components/side-nav/toggle-button.tsx
+++ packages/swingset-theme-hashicorp/src/components/side-nav/toggle-button.tsx
@@ -7,12 +7,15 @@ type ToggleButtonProps = {
 }
 
 export function ToggleButton({ toggle, isOpen }: ToggleButtonProps) {
+  const ariaLabel = isOpen
+    ? 'Collapse navigation menu'
+    : 'Expand navigation menu'
   return (
     <button
-      aria-label="Toggle the navigation menu between open and closed states"
+      aria-label={ariaLabel}
       className={cx(
-        'ss-bg-surface-faint ss-hidden lg:ss-block hover:ss-bg-surface-action ss-opacity-70 hover:ss-text-foreground-action ss-border-2 ss-border-faint hover:ss-border-action ss-p-1 ss-rounded-full hover:ss-shadow-lg ss-transition-all ss-shadow-sm ss-fixed ss-top-6 ss-left-72',
-        !isOpen && 'ss--translate-x-[17rem]'
+        'ss-bg-surface-faint ss-hidden lg:ss-block hover:ss-bg-surface-action ss-opacity-70 hover:ss-text-foreground-action ss-border-2 ss-border-faint hover:ss-border-action ss-ml-1 ss-p-1 ss-rounded-full hover:ss-shadow-lg ss-transition-all ss-shadow-sm ss-fixed ss-top-6 ss-left-72',
+        !isOpen && 'ss--translate-x-72'
       )}
       onClick={toggle}
     >
