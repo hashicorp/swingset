62939ae3ddb6ebff1fe1425a6ab1fda97311933c
Preston Bourne <preston.bourne@hashicorp.com>
2023-07-07 03:01:39 -0400

    `react-live` example

--- packages/swingset-theme-hashicorp/src/components/live-component/index.tsx
+++ packages/swingset-theme-hashicorp/src/components/live-component/index.tsx
@@ -1,7 +1,144 @@
-import React from 'react'
+'use client'
+import { ReactElement, useState } from 'react'
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 
-function LiveComponent() {
-  return <div>livecomponent</div>
+function LiveComponent({
+  children,
+  components,
+  collapsed = false,
+  scope,
+}: {
+  children: string
+  components: Record<string, ReactElement>
+  collapsed: boolean
+  scope: any
+}) {
+  // const id = createId(children)
+  // const componentName = Object.keys(scope)[0]
+  const [code, setCode] = useState(children)
+  const [isCollapsed, setIsCollapsed] = useState(collapsed)
+
+  return (
+    <div>
+      <div>Share</div>
+      <LiveProvider
+        code={code}
+        scope={scope}
+        transformCode={(code) => {
+          setCode(code)
+          return code
+        }}
+      >
+        <LivePreview />
+        <h6
+          // className={`${sg.label} ${s.clickable}`}
+          onClick={() => setIsCollapsed(!isCollapsed)}
+        >
+          Code Editor
+          <svg
+            width="15"
+            height="10.5"
+            viewBox="0 0 10 7"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M8.05264 2L5.05264 5.00649L2.05264 2"
+              stroke="#aaa"
+              strokeWidth="1.25"
+              strokeLinecap="square"
+            ></path>
+          </svg>
+        </h6>
+        <div>
+          <LiveEditor />
+        </div>
+        <LiveError />
+      </LiveProvider>
+    </div>
+  )
 }
 
 export { LiveComponent }
+
+/*
+import sg from '../shared.module.css'
+import s from './style.module.css'
+import { ReactElement, useState } from 'react'
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
+import theme from '../../utils/editor-theme'
+import createId from '../../utils/create-id'
+import { useRestoreUrlState, setUrlState } from '../../utils/url-state'
+import scrollToElement from '../../utils/scroll-to-element'
+
+export default function createLiveComponent(
+  scope: Record<string, ReactElement>
+) {
+  return function LiveComponent({
+    children,
+    components,
+    collapsed = false,
+  }: {
+    children: string
+    components: Record<string, ReactElement>
+    collapsed: boolean
+  }) {
+    const id = createId(children)
+    const componentName = Object.keys(scope)[0]
+    const [code, setCode] = useState(children)
+    const [isCollapsed, setIsCollapsed] = useState(collapsed)
+
+    // restore saved state if present
+    useRestoreUrlState((qs) => {
+      if (qs.id == id) {
+        setCode(qs.values)
+        setTimeout(scrollToElement.bind(null, id), 100)
+      }
+    })
+
+    return (
+      <div className={s.liveComponent} id={id}>
+        <div className={sg.save} onClick={() => setUrlState(id, code, true)}>
+          Share
+        </div>
+        <LiveProvider
+          code={code}
+          scope={Object.assign({}, scope, components)}
+          theme={theme}
+          transformCode={(code) => {
+            setCode(code)
+            return code
+          }}
+        >
+          <LivePreview />
+          <h6
+            className={`${sg.label} ${s.clickable}`}
+            onClick={() => setIsCollapsed(!isCollapsed)}
+          >
+            Code Editor
+            <svg
+              width="15"
+              height="10.5"
+              viewBox="0 0 10 7"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className={`${s.caret} ${isCollapsed ? s.caretCollapsed : ''}`}
+            >
+              <path
+                d="M8.05264 2L5.05264 5.00649L2.05264 2"
+                stroke="#aaa"
+                strokeWidth="1.25"
+                strokeLinecap="square"
+              ></path>
+            </svg>
+          </h6>
+          <div className={`${s.editor} ${isCollapsed ? s.collapsed : ''}`}>
+            <LiveEditor />
+          </div>
+          <LiveError />
+        </LiveProvider>
+      </div>
+    )
+  }
+}
+*/
