561be3c19251b61aa0cd618aac1df1368a41e51b
Preston Bourne <preston.bourne@hashicorp.com>
2023-07-11 14:04:14 -0400

    remove unused comment

--- packages/swingset-theme-hashicorp/src/components/live-component/get-file-map.ts
+++ packages/swingset-theme-hashicorp/src/components/live-component/get-file-map.ts
@@ -2,23 +2,18 @@ import type { SandpackFile } from '@codesandbox/sandpack-react'
 import { parseCode } from '../code-block/helpers'
 
 export const getFileMap = (codeSnippets: React.ReactElement[]) => {
-  //Handle Logic is only one codeSnippet is passed in...better DX
-
   const fileMap = codeSnippets.reduce(
     (result: Record<string, SandpackFile>, codeSnippet: React.ReactElement) => {
-      //TODO: Throw error to ensure this is only used with a <pre> tag
-
       const { props } = codeSnippet
 
       const { filePath, hidden, active, children } = props
 
       const code = parseCode(children)
-
+      //TODO: LINK TO README, CodeSandbox docs aren't very helpful here
       if (!filePath) {
         throw new Error(
           `<CodeBlock/> is missing a fileName prop. See docs: https://sandpack.codesandbox.io/docs/getting-started/usage#files `
         )
-        //TODO: LINK TO README, CodeSandbox docs aren't very helpful here
       }
 
       if (!!result[filePath]) {
