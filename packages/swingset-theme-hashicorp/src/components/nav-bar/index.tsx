8dc71b8d69c01b84eb0824cc5275005ced82c061
Preston Bourne <preston.bourne@hashicorp.com>
2023-07-19 15:43:38 -0400

    New UI ✨

---
+++ packages/swingset-theme-hashicorp/src/components/nav-bar/index.tsx
@@ -0,0 +1,37 @@
+import { Logo } from './logo'
+import Link from 'next/link'
+import { IconGithub24 } from '@hashicorp/flight-icons/svg-react'
+
+function NavBar() {
+  return (
+    <header className="ss-w-full ss-border-b-2 ss-border-faint">
+      <nav>
+        <ul className="ss-px-6 ss-p-2 ss-flex ss-items-center ss-w-full ss-justify-between">
+          <li className="ss-flex-1">
+            <Logo />
+          </li>
+          <li className="ss-flex-1 ss-text-center">
+            <Link
+              href={'/swingset'}
+              className="ss-font-semibold ss-text-dark hover:ss-text-action"
+            >
+              About
+            </Link>
+          </li>
+
+          <li className="ss-flex-1">
+            <Link
+              href={'https://github.com/hashicorp/swingset'}
+              className="ss-font-semibold ss-text-dark hover:ss-text-action"
+              target="_blank"
+            >
+              <IconGithub24 className="ss-ml-auto" />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export { NavBar }
---
+++ packages/swingset-theme-hashicorp/src/components/nav-bar/logo.tsx
@@ -0,0 +1,21 @@
+import { IconHashicorp24 } from '@hashicorp/flight-icons/svg-react'
+
+function Logo() {
+  return (
+    <section className="ss-flex ss-items-center ss-gap-4">
+      <picture className="ss-border-2 ss-h-full ss-p-2 ss-border-faint ss-rounded-lg">
+        <IconHashicorp24 />
+      </picture>
+      <div>
+        <p className="ss-font-semibold ss-text-dark ss-my-0 ss-text-sm">
+          HashiCorp
+        </p>
+        <p className="ss-font-extralight ss-leading-4 ss-my-0 ss-text-sm">
+          Web Presence
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export { Logo }
