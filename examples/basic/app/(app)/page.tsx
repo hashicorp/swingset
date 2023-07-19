d4ffd220af0ab4df756f3d8ca27ad42acfa3c067
Preston Bourne <preston.bourne@hashicorp.com>
2023-07-19 16:42:44 -0400

    _

--- examples/basic/app/(app)/page.tsx
+++ examples/basic/app/(app)/page.tsx
@@ -1,3 +1,9 @@
+'use client'
+//Once this page starts to get built, make this can be server component again
 export default function Index() {
+  if (typeof window !== 'undefined') {
+    window.location.replace('https://swingset-example.vercel.app/swingset')
+  }
+
   return <h1>Hello world</h1>
 }
