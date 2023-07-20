3c6e3374a47f52edf1bc41be317a45caecf8c55e
Preston Bourne <preston.bourne@hashicorp.com>
2023-07-20 14:08:41 -0400

    add `useEffect`

--- examples/basic/app/(app)/page.tsx
+++ examples/basic/app/(app)/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 /*Once this page starts to get built, make this can be server component again */
 
 export default function Index() {
   const router = useRouter()
-  router.push('/swingset')
+  useEffect(() => router.push('/swingset'), [])
 
   return <h1>Hello world</h1>
 }
