4365206a1e0f476b2933420c17d8a738c68d5025
Preston Bourne <preston.bourne@hashicorp.com>
2023-07-20 14:15:56 -0400

    _

--- examples/basic/app/(app)/page.tsx
+++ examples/basic/app/(app)/page.tsx
@@ -4,8 +4,10 @@ import { useEffect } from 'react'
 /*Once this page starts to get built, make this can be server component again */
 
 export default function Index() {
-  const router = useRouter()
-  useEffect(() => router.push('/swingset'), [])
+  useEffect(() => {
+    const router = useRouter()
+    router.push('/swingset')
+  }, [])
 
   return <h1>Hello world</h1>
 }
