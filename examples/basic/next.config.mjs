5da4ecaa3fe18df9cc43355ed233af40e8948427
Preston Bourne <preston.bourne@hashicorp.com>
2023-07-26 17:58:34 -0400

    anchor headings

--- examples/basic/next.config.mjs
+++ examples/basic/next.config.mjs
@@ -1,12 +1,14 @@
 import withSwingset from 'swingset'
 import remarkGfm from 'remark-gfm'
 import rehypeMdxCodeProps from 'rehype-mdx-code-props'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
 
 export default withSwingset({
   componentRootPattern: './components',
   theme: 'swingset-theme-hashicorp',
   remarkPlugins: [remarkGfm],
-  rehypePlugins: [rehypeMdxCodeProps],
+  rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, rehypeMdxCodeProps],
 })({
   experimental: {
     appDir: true,
